bbff7e0a036e8f64924e043b35176684a66dee60
Anthony Reyes Castañeda <s106515@ap.be>
2025-12-30 19:26:43 +0000

    cloudsystemen-project zogoed als klaar.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Zet EJS als template engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+// Route
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+// Start server
+app.listen(port, '0.0.0.0', () => console.log(`App listening on port ${port}`));
